8b20b9faedb1e63877f1d4d65d360f1971e360cb
Vpekdas <volkan.pekdas@hotmail.com>
2026-03-31 03:18:43 +0200

    feat: Prototype of A*. TODO: Test in not flat map.

--- src/Entity/Player.cpp
+++ src/Entity/Player.cpp
@@ -186,26 +186,6 @@ void Player::tick(float delta)
         return;
     }
 
-    bool is_attack_pressed = Input::is_action_pressed("attack");
-
-    if (is_attack_pressed && !m_was_attack_pressed)
-    {
-        Ray ray(
-            m_camera->get_global_transform().position(),
-            m_camera->get_global_transform().forward());
-
-        float attack_range = 5.0f;
-
-        auto hit = m_world->raycast_entities(ray, attack_range, this);
-
-        if (hit)
-        {
-            hit->entity->call_rpc("on_hit", *this);
-        }
-    }
-
-    m_was_attack_pressed = is_attack_pressed;
-
     Transform3D transform = m_transform;
 
     const glm::vec3 up(0.0, 1.0, 0.0);
