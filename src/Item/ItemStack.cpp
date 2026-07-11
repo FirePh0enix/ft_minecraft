270dbb774453f597840d13043c6501eca3ab17ef
FirePh0enix <leonard@delbecq.net>
2026-07-11 14:45:51 +0200

    Fix stack reset when going over 63

--- src/Item/ItemStack.cpp
+++ src/Item/ItemStack.cpp
@@ -5,8 +5,8 @@ Option<ItemStack> ItemStack::merge(const ItemStack& stack)
     if (m_item != stack.m_item || m_count >= itemstack_max_size)
         return stack;
 
-    size_t excess = count() + stack.count() > itemstack_max_size ? (count() + stack.count()) % itemstack_max_size : 0;
-    m_count = (count() + stack.count()) % itemstack_max_size;
+    size_t excess = count() + stack.count() > itemstack_max_size ? (count() + stack.count()) % (itemstack_max_size + 1) : 0;
+    m_count = (count() + stack.count()) % (itemstack_max_size + 1);
 
     if (excess > 0)
     {
@@ -21,7 +21,7 @@ Option<ItemStack> ItemStack::merge(const ItemStack& stack)
 
 void ItemStack::sub(size_t count)
 {
-    if (count >= m_count)
+    if ((ssize_t)m_count - (ssize_t)count < 0)
     {
         m_item = Id<Item>();
         m_count = 0;
