b2947b3ad6056bc0f83a411a192960b4713e817b
FirePh0enix <leonard@delbecq.net>
2026-08-14 14:36:46 +0200

    fix compilation

--- src/Entity/Arrow.cpp
+++ src/Entity/Arrow.cpp
@@ -4,12 +4,12 @@
 #include "Engine.hpp"
 #include "Entity/Mob.hpp"
 #include "Render/Renderer.hpp"
-#include "glm/ext/vector_float3.hpp"
+
 #include <memory>
 
 ArrowEntity::ArrowEntity(Id<Item> item) : m_item(item)
 {
-    m_aabb = AABB(-glm::vec3(0.04f, 0.04f, 0.375f), glm::vec3(0.04f, 0.04f, 0.375f));
+    m_aabb = AABBf(-glm::vec3(0.04f, 0.04f, 0.375f), glm::vec3(0.04f, 0.04f, 0.375f));
     std::shared_ptr<Block> block = Engine::get().registry().block_from_item(item);
     m_textures = glm::uvec3(block->get_texture_ids()[0] | (block->get_texture_ids()[1] << 16), block->get_texture_ids()[2] | (block->get_texture_ids()[3] << 16), block->get_texture_ids()[4] | (block->get_texture_ids()[5] << 16));
 }
@@ -35,7 +35,7 @@ void ArrowEntity::tick(float delta)
 
     move_and_collide();
 
-    AABB search_box = AABB::from_center_extent(get_global_transform().position(), glm::vec3(0.2f));
+    AABBf search_box = AABBf::from_center_extent(get_global_transform().position(), glm::vec3(0.2f));
 
     auto entities = m_world->get_dimension(m_dimension).cast_box(search_box);
     for (const auto& e : entities)
